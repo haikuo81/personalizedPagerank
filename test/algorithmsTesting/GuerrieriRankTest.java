8bbd8e62bc857bf7616cd8f40d794ef33eaf5140
jacopo <jacopo@jacopo.jacopo>
2017-02-08 12:34:16 +0100

    more tests for guerrierirank

--- test/algorithmsTesting/GuerrieriRankTest.java
+++ test/algorithmsTesting/GuerrieriRankTest.java
@@ -55,7 +55,7 @@ public class GuerrieriRankTest extends TestCase
         
     }
     
-    public void testSingleNode()
+    public void testGetters()
     {
         DirectedGraph<Integer, DefaultEdge> g = new DefaultDirectedGraph(DefaultEdge.class);
         
@@ -63,6 +63,95 @@ public class GuerrieriRankTest extends TestCase
         
         PersonalizedPageRankAlgorithm res = new GuerrieriRank(g);
         
+        //node doesn't exist
+        try 
+        {
+            res.getMap(4);
+            fail("this line shouldn't be reached");
+        } 
+        catch (IllegalArgumentException e) {}
+        
+        //origin doesn't exist
+        try 
+        {
+            res.getRank(4, 1);
+            fail("this line shouldn't be reached");
+        } 
+        catch (IllegalArgumentException e) {}
+        
+        //target doesn't exist
+        try 
+        {
+            res.getRank(1, 4);
+            fail("this line shouldn't be reached");
+        } 
+        catch (IllegalArgumentException e) {}
+        
+        Map<Integer, Double> map1 = res.getMap(1);
+        Map<Integer, Map<Integer, Double>> map2 = res.getMaps();
+        
+        assertEquals(map1.size(), 1, 0);
+        assertEquals(map2.size(), 1, 0);
+        assertEquals(map2.get(1).size(), 1, 0);
+        //cant use assertSame because getmap and getMaps use Collections.unmodifiableMap
+        assertEquals(map1, map2.get(1));
         
     }
+    
+    public void testEmptyGraph()
+    {
+        DirectedGraph<Integer, DefaultEdge> g = new DefaultDirectedGraph(DefaultEdge.class);
+        
+        PersonalizedPageRankAlgorithm res = new GuerrieriRank(g);
+        
+        assertEquals(res.getMaps().size(), 0, 0);
+    }
+    
+    public void testSingleNodeGraph()
+    {
+        DirectedGraph<Integer, DefaultEdge> g = new DefaultDirectedGraph(DefaultEdge.class);
+        
+        //1 node no edges
+        g.addVertex(1);
+        PersonalizedPageRankAlgorithm res = new GuerrieriRank(g, 10, 10, 0.5);
+        assertEquals(res.getMaps().size(), 1, 0);
+        assertEquals(res.getMap(1).size(), 1, 0);
+        //expected is not 1 because as of now nodes without edges don't always teleport
+        assertEquals((Double) res.getRank(1, 1), 0.5, 0);
+        
+        //1 node 1 edge to himself
+        g.addEdge(1, 1);
+        res = new GuerrieriRank(g);
+        assertEquals(res.getMaps().size(), 1, 0);
+        assertEquals(res.getMap(1).size(), 1, 0);
+        assertEquals((Double) res.getRank(1, 1), 1, 0);
+    }
+    
+    public void testTwoNodesGraph()
+    {
+        DirectedGraph<Integer, DefaultEdge> g = new DefaultDirectedGraph(DefaultEdge.class);
+        
+        g.addVertex(1);
+        g.addVertex(2);
+        
+        //no edges first
+        PersonalizedPageRankAlgorithm<Integer, Double> res = new GuerrieriRank(g, 10, 100);
+        
+        assertEquals(res.getRank(1, 2), 0, 0);
+        assertEquals(res.getRank(2, 1), 0, 0);
+        
+        //with edges
+        g.addEdge(1, 2);
+        g.addEdge(2, 1);
+        
+        res = new GuerrieriRank(g, 10, 10000);
+        assertEquals(res.getMaps().size(), 2, 0);
+        assertEquals(res.getMap(1).size(), 2, 0);
+        assertEquals(res.getMap(2).size(), 2, 0);
+        
+        assertTrue(res.getRank(1, 1) >= res.getRank(1, 2)); 
+        assertTrue(res.getRank(2, 2) >= res.getRank(2,1));
+    }
+    
+    
 }
