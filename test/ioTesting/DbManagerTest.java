5a89d36ffc8dee5323db70244dd336efdc7331a4
fruttasecca <jacopo.gobbi@studenti.unitn.it>
2017-05-10 12:02:26 +0200

    updated tests to test both psql and mysql

--- test/ioTesting/DbManagerTest.java
+++ test/ioTesting/DbManagerTest.java
@@ -6,18 +6,45 @@ import benchmarking.Result;
 import io.DbManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import javax.json.Json;
+import javax.json.JsonObject;
 import junit.framework.TestCase;
 
 public class DbManagerTest extends TestCase 
 {
-    DbManager db = new DbManager();
-    
-    public void testInsertAlgorithm()
+    public void testConstructor()
     {
+        try 
+        {
+            new DbManager("randomDBThatShouldntExist", "t", "t", "t");
+            fail("this line shouldn't be reached");
+        } 
+        catch (IllegalArgumentException e) {}
+        
+        //these should work (shouldn't throw exceptions)
+        try
+        {
+            DbManager db1 = new DbManager("postgres", "ppr", "ppr", "ppr");
+            DbManager db2 = new DbManager("mysql", "t", "t", "t");
+        }
+        catch (IllegalArgumentException e)
+        {
+            fail("this line shouldn't be reached");
+        }
+    }
+    
+    
+    public void testPSQLInsertAlgorithm()
+    {
+        DbManager db = new DbManager("postgres", "ppr", "ppr", "ppr");
+        JsonObject params = Json.createObjectBuilder()
+                .add("testP1", "description")
+                .add("testP2", "")
+                .build();
         db.query("DELETE FROM RUNS WHERE graph = 'testGraph'");
         db.query("DELETE FROM GRAPHS WHERE name = 'testGraph'");
         db.query("DELETE FROM ALGORITHMS WHERE name = 'testAlgorithm'");
-        db.insertAlgorithm("testAlgorithm", 0);
+        db.insertAlgorithm("testAlgorithm", params);
         
         try (PreparedStatement st = db.getStatement("SELECT * FROM ALGORITHMS WHERE name = ?"))
         {
@@ -28,7 +55,7 @@ public class DbManagerTest extends TestCase
                 if(rs.next())
                 {
                     assertEquals("testAlgorithm", rs.getString("name"));
-                    assertEquals(0, rs.getInt("numberOfParameters"));
+                    assertEquals(params.toString(), rs.getString("params"));
                 }
                 else
                     fail();
@@ -42,8 +69,9 @@ public class DbManagerTest extends TestCase
         db.query("DELETE FROM ALGORITHMS WHERE name = 'testAlgorithm'");
     }
     
-    public void testInsertGraph()
+    public void testPSQLInsertGraph()
     {
+        DbManager db = new DbManager("postgres", "ppr", "ppr", "ppr");
         db.query("DELETE FROM RUNS WHERE graph = 'testGraph'");
         db.query("DELETE FROM GRAPHS WHERE name = 'testGraph'");
         db.query("DELETE FROM ALGORITHMS WHERE name = 'testAlgorithm'");
@@ -76,9 +104,13 @@ public class DbManagerTest extends TestCase
     }
     
     
-    public void testInsertRun()
+    public void testPSQLInsertRun()
     {
-        double[] params = {2, 3, 4.0, -0.3};
+        DbManager db = new DbManager("postgres", "ppr", "ppr", "ppr");
+        JsonObject params = Json.createObjectBuilder()
+                .add("testP1", "description")
+                .add("testP2", "")
+                .build();
         double[] in = {0d, 0.5,0.9,0.2};
         Result r1 = new Result(in);
         Result r2 = new Result(in);
@@ -89,7 +121,7 @@ public class DbManagerTest extends TestCase
         db.query("DELETE FROM RUNS WHERE graph = 'testGraph'");
         db.query("DELETE FROM GRAPHS WHERE name = 'testGraph'");
         db.query("DELETE FROM ALGORITHMS WHERE name = 'testAlgorithm'");
-        db.insertAlgorithm("testAlgorithm", 4);
+        db.insertAlgorithm("testAlgorithm", params);
         db.insertGraph("testGraph", 10, 20, true, false);
         db.insertRun("testGraph", "testAlgorithm", "testCpu", 50, params, data, 1337);
         
@@ -107,9 +139,136 @@ public class DbManagerTest extends TestCase
                     assertEquals("testCpu", rs.getString("cpu"));
                     assertEquals(50, rs.getInt("sampleNodes"));
                     assertEquals(10, rs.getInt("topK"));
-                    Double[] tmp = (Double[])(rs.getArray("params")).getArray();
-                    for(int i = 0; i < params.length; i++)
-                        assertEquals(tmp[i], params[i], 0.0001);
+                    assertEquals(params.toString(), rs.getString("params"));
+                    assertEquals(50, rs.getInt("jaccardAverage"));
+                    assertEquals(r1.getMin(), rs.getFloat("jaccardMin"), 0.0001);
+                    assertEquals(r1.getStd(), rs.getFloat("jaccardStd"), 0.0001);
+                    assertEquals(50, rs.getInt("kendallAverage"), 0.0001);
+                    assertEquals(r1.getMin(), rs.getFloat("KendallMin"), 0.0001);
+                    assertEquals(r1.getStd(), rs.getFloat("KendallStd"), 0.0001);
+                    assertEquals(1337, rs.getInt("runTime"));
+                }
+                else
+                    fail();
+            }
+        }
+        catch (Exception e) 
+        {
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        
+        db.query("DELETE FROM RUNS WHERE graph = 'testGraph'");
+        db.query("DELETE FROM GRAPHS WHERE name = 'testGraph'");
+        db.query("DELETE FROM ALGORITHMS WHERE name = 'testAlgorithm'");
+    }
+    
+    public void testMYSQLInsertAlgorithm()
+    {
+        DbManager db = new DbManager("postgres", "ppr", "ppr", "ppr");
+        JsonObject params = Json.createObjectBuilder()
+                .add("testP1", "description")
+                .add("testP2", "")
+                .build();
+        db.query("DELETE FROM RUNS WHERE graph = 'testGraph'");
+        db.query("DELETE FROM GRAPHS WHERE name = 'testGraph'");
+        db.query("DELETE FROM ALGORITHMS WHERE name = 'testAlgorithm'");
+        db.insertAlgorithm("testAlgorithm", params);
+        
+        try (PreparedStatement st = db.getStatement("SELECT * FROM ALGORITHMS WHERE name = ?"))
+        {
+            st.setString(1, "testAlgorithm");
+            
+            try (ResultSet rs = st.executeQuery())
+            {
+                if(rs.next())
+                {
+                    assertEquals("testAlgorithm", rs.getString("name"));
+                    assertEquals(params.toString(), rs.getString("params"));
+                }
+                else
+                    fail();
+            }
+        }
+        catch (Exception e) 
+        {
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        db.query("DELETE FROM ALGORITHMS WHERE name = 'testAlgorithm'");
+    }
+    
+    public void testMYSQLInsertGraph()
+    {
+        DbManager db = new DbManager("postgres", "ppr", "ppr", "ppr");
+        db.query("DELETE FROM RUNS WHERE graph = 'testGraph'");
+        db.query("DELETE FROM GRAPHS WHERE name = 'testGraph'");
+        db.query("DELETE FROM ALGORITHMS WHERE name = 'testAlgorithm'");
+        db.insertGraph("testGraph", 10, 20, true, false);
+        
+        try (PreparedStatement st = db.getStatement("SELECT * FROM GRAPHS WHERE name = ?"))
+        {
+            st.setString(1, "testGraph");
+            
+            try (ResultSet rs = st.executeQuery())
+            {
+                if(rs.next())
+                {
+                    assertEquals("testGraph", rs.getString("name"));
+                    assertEquals(10, rs.getInt("vertices"));
+                    assertEquals(20, rs.getInt("edges"));
+                    assertTrue(rs.getBoolean("directed"));
+                    assertTrue(!rs.getBoolean("bipartite"));
+                }
+                else
+                    fail();
+            }
+        }
+        catch (Exception e) 
+        {
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        db.query("DELETE FROM GRAPHS WHERE name = 'testGraph'");
+    }
+    
+    
+    public void testMYSQLInsertRun()
+    {
+        DbManager db = new DbManager("postgres", "ppr", "ppr", "ppr");
+        JsonObject params = Json.createObjectBuilder()
+                .add("testP1", "description")
+                .add("testP2", "")
+                .build();
+        double[] in = {0d, 0.5,0.9,0.2};
+        Result r1 = new Result(in);
+        Result r2 = new Result(in);
+        PersonalizedPageRankAlgorithm.Parameters p1 = new PersonalizedPageRankAlgorithm.Parameters(0, 1, 2, 3d, 4d);
+        PersonalizedPageRankAlgorithm.Parameters p2 = new PersonalizedPageRankAlgorithm.Parameters(0, 1, 2, 3d, 4d);
+        ComparisonData data = new ComparisonData(10, r1, r2, p1, p2);
+
+        db.query("DELETE FROM RUNS WHERE graph = 'testGraph'");
+        db.query("DELETE FROM GRAPHS WHERE name = 'testGraph'");
+        db.query("DELETE FROM ALGORITHMS WHERE name = 'testAlgorithm'");
+        db.insertAlgorithm("testAlgorithm", params);
+        db.insertGraph("testGraph", 10, 20, true, false);
+        db.insertRun("testGraph", "testAlgorithm", "testCpu", 50, params, data, 1337);
+        
+        //inserting 
+        try (PreparedStatement st = db.getStatement("SELECT * FROM RUNS WHERE graph = ?"))
+        {
+            st.setString(1, "testGraph");
+            
+            try (ResultSet rs = st.executeQuery())
+            {
+                if(rs.next())
+                {
+                    assertEquals("testGraph", rs.getString("graph"));
+                    assertEquals("testAlgorithm", rs.getString("algorithm"));
+                    assertEquals("testCpu", rs.getString("cpu"));
+                    assertEquals(50, rs.getInt("sampleNodes"));
+                    assertEquals(10, rs.getInt("topK"));
+                    assertEquals(params.toString(), rs.getString("params"));
                     assertEquals(50, rs.getInt("jaccardAverage"));
                     assertEquals(r1.getMin(), rs.getFloat("jaccardMin"), 0.0001);
                     assertEquals(r1.getStd(), rs.getFloat("jaccardStd"), 0.0001);
